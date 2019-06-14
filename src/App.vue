acba3bf94a8b1e66e1c998b8f84d9fb4884e2fd0
Victoria Chen <Victoria Chen>
2019-06-15 00:02:06 +0200

    show chart with average salaries for each department

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+        <canvas id="myChart" width="400" height="400">chart here</canvas>
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 
 
-    
 
 
   </div>
@@ -12,14 +13,14 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Chart from 'chart.js';
 
 export default {
   name: 'app',
   props: {
     allData: [],
-    listOfAllDepartments: [],
-    departmentStats: []
-  },
+    listOfAllDepartments: []
+      },
   components: {
     HelloWorld
   },
@@ -52,6 +53,7 @@ export default {
               console.log( "all data: ", this.allData );
      
             this.createListOfAllDepartments();
+            this.drawChart();
 
           });
 
@@ -64,7 +66,7 @@ export default {
         this.allData.forEach( empleado => {
 
           var duplicate = allDeparts.find( depart => depart.department == empleado.Departamento );
-          console.log( "duplicate: " , duplicate );
+          // console.log( "duplicate: " , duplicate );
           if( duplicate != undefined ){
             duplicate.TotalEmployees++;
             duplicate.totalSalary += empleado.Sueldo;
@@ -73,8 +75,8 @@ export default {
             obj.department = empleado.Departamento;
             obj.TotalEmployees = 1;
             obj.totalSalary = empleado.Sueldo;
-            console.log( obj );
-            // allDeparts.push( item.Departamento );
+            // console.log( obj );
+  
             allDeparts.push( obj );
           }
         });
@@ -83,6 +85,59 @@ export default {
 
     },
 
+  drawChart()
+  { 
+
+    // order list here
+    let deparmentNames = [];
+    let avgSalary = [];
+    this.listOfAllDepartments.forEach( department => {
+      deparmentNames.push( department.department );
+      avgSalary.push( department.totalSalary/department.TotalEmployees );
+    })
+
+
+
+    console.log( deparmentNames );
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: deparmentNames,
+        datasets: [{
+            label: '# of Votes',
+            data: avgSalary,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+  }
  
  
   }
